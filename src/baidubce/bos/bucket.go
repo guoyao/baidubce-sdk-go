8f6ba86b9c8e8391e9a1a2161ccc978ed61d4770
guoyao <guoyao.dev@gmail.com>
2016-05-09 21:06:15 +0800

    change bucket owner to struct type

--- src/baidubce/bos/bucket.go
+++ src/baidubce/bos/bucket.go
@@ -9,14 +9,19 @@ type Location struct {
 	LocationConstraint string
 }
 
-// BucketSummary is a struct for bucket summary.
-type BucketSummary struct {
-	Owner   map[string]string
-	Buckets []Bucket
+type BucketOwner struct {
+	Id          string
+	DisplayName string
 }
 
 // Bucket is a struct for bucket info.
 type Bucket struct {
 	Name, Location string
 	CreationDate   time.Time
+}
+
+// BucketSummary is a struct for bucket summary.
+type BucketSummary struct {
+	Owner   BucketOwner
+	Buckets []Bucket
 }
